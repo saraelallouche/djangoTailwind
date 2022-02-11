564bde2b7b1a4572d1e59aaa4f2bc469c1c2100c
jlkhermes38 <jlkhermes38@gmail.com>
2022-02-11 01:24:24 +0100

    adding forms

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, HeroSectionsPageView, FormLayoutsPageView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
+    path("hero_sections", HeroSectionsPageView.as_view(), name="hero_sections"),
+    path("form_layouts", FormLayoutsPageView.as_view(), name="form_layout"),
+
 ]
--- snippets/views.py
+++ snippets/views.py
@@ -5,3 +5,9 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = "home/landing_page.html"
+
+class HeroSectionsPageView(TemplateView):
+    template_name = "hero_sections.html"
+
+class FormLayoutsPageView(TemplateView):
+    template_name = "form_layout.html"
