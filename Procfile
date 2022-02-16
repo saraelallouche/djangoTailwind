e7bd0fd5c4e6b137df5f5651d5091b4af793488a
jlkhermes38 <jlkhermes38@gmail.com>
2022-02-16 23:18:03 +0100

    heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn django_tailwind.wsgi --timeout 120 --keep-alive 5 --logfile --log-level debug --preload
--- run_manage.sh
+++ run_manage.sh
@@ -2,5 +2,8 @@ echo "Running makemigrations"
 python manage.py makemigrations
 echo "Running migrate"
 python manage.py migrate
+echo "Running collectstatics"
+python manage.py collectstatic --noinput
 echo "Running server"
 python manage.py runserver
+
