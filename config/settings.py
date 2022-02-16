f0a7a980226597f8f9f4642713144c9613faba42
jlkhermes38 <jlkhermes38@gmail.com>
2022-02-17 00:11:05 +0100

    fixing database

--- config/settings.py
+++ config/settings.py
@@ -35,9 +35,6 @@ DEBUG = env('DEBUG')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
