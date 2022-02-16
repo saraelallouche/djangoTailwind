0c6f0825ed6f2dc004735fcf88c3e8cd96108169
jlkhermes38 <jlkhermes38@gmail.com>
2022-02-17 00:02:46 +0100

    fixing database

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # False if not in os.environ
-DEBUG = env('DEBUG')
+DEBUG = env('DEBUG',cast=bool)
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
