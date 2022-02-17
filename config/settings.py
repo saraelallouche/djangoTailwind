03e852c77b5a54b6545a99e344d91540145ce92b
jlkhermes38 <jlkhermes38@gmail.com>
2022-02-18 00:12:54 +0100

    adding static_cdn

--- config/settings.py
+++ config/settings.py
@@ -159,14 +159,12 @@ try:
 except:
     pass
 
-NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = "static/"
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = "static_cdn/"
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
